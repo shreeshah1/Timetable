cee5d547bf145913d74a3c57415219ed8ccc35fe
Shree Shah <shreeshah@Shrees-MacBook-Pro.local>
2020-08-27 00:53:38 -0400

    First commit

---
+++ android/app/src/main/java/com/myreactnative/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.myreactnative.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
